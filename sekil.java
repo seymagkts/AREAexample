5d9688ef5867671303f771e7efcc190ad21f8592
Seyma <63148309+lacilobaid@users.noreply.github.com>
2021-09-03 15:27:54 +0300

    Create sekil.java

---
+++ sekil.java
@@ -0,0 +1,17 @@
+public abstract class sekil {
+    private String isim;
+
+    public sekil(String isim){
+        this.setIsim(isim);
+    }
+
+    abstract void alanHesapla();
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+}
